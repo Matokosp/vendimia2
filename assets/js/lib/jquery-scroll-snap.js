988f63449612fd9723095657b9a287c01ce88d02
mati <jmsalinas@uc.cl>
2019-01-27 22:57:27 -0300

    updates css

--- assets/js/lib/jquery-scroll-snap.js
+++ assets/js/lib/jquery-scroll-snap.js
@@ -27,13 +27,13 @@
         if(
           scroll <= $( this ).offset().top && (
             // advance
-            ( direction == 'down' && scroll >= $( this ).offset().top - ( windowHeight * 2 / 3 ) ) ||
+            ( direction == 'down' && scroll >= $( this ).offset().top - ( windowHeight * 1 / 4 ) ) ||
             // stabilize
             ( direction == 'up' && scroll >= $( this ).offset().top - ( windowHeight / 3 ) )
           ) ||
           scroll >= $( this ).offset().top && (
             // advance
-            ( direction == 'up' && scroll <= $( this ).offset().top + ( windowHeight * 2 / 3 ) ) ||
+            ( direction == 'up' && scroll <= $( this ).offset().top + ( windowHeight * 1 / 4 ) ) ||
             // stabilize
             ( direction == 'down' && scroll <= $( this ).offset().top + ( windowHeight / 3 ) )
           )
