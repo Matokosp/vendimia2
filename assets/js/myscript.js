4d3bc9d83f9dd980776269b9e9e69d2f8753b080
mati <jmsalinas@uc.cl>
2019-02-05 20:31:19 -0300

    update scroll

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -20,39 +20,42 @@
 	var menuicon = $('.menuicon');
 	var $window = $(window);
 	var contentUno = $('.content-01').offset();
-	// var contentDos = $('.content-02').offset();
+	var contentDos = $('.content-02').offset();
 	// var contentTres = $('.content-03').offset();
 	var contentCuatro = $('#contentCuatro').offset();
-	var infoCasas = $('.info__casas').offset();
 	var contentCinco = $('#contentCinco').offset();
-	var contentSeis = $('.content-06').offset();
+	var contentSeis = $('#contentSeis').offset();
 	// var contentSiete = $('.content-07').offset();
 	// var contentOcho = $('.content-08').offset();
 
 	const mq = window.matchMedia( "(max-width: 768px)" );
+	var windowHeight = $( window ).height();
+
 
 	$window.scroll(function() {
 		if ($window.scrollTop() >= contentUno.top ) {
 			menuicon.removeClass('menuicon--white');
-			$('.info__casas-item').removeClass('info__casas-item--reveal');
+			$('.info__casas-item').removeClass('reveal');
 			// menuicon.css('background-color', '#000');
 		}
-		if ($window.scrollTop()+50 >= contentCuatro.top ) {
+
+		if ($window.scrollTop() >= contentDos.top - ( windowHeight * 2 / 8 ) ) {
+			$('.origen__proyecto--img').addClass('reveal');
+			$('.origen__proyecto').children('p').addClass('reveal');
+		}
+
+		if ($window.scrollTop()+100 >= contentCuatro.top ) {
 			menuicon.addClass('menuicon--white');
-			$('.info__casas-item').addClass('info__casas-item--reveal');
+			$('.info__casas-item').addClass('reveal');
 			// menuicon.css('background-color', '#fff');
 		}
 
-		// if (mq.matches) {
-		// 	// if ($window.scrollTop() >= infoCasas.top ) {
-		// 	// 	menuicon.css('background-color', '#000');
-		// 	// }
-		// }
-		if ($window.scrollTop()+50 >= contentCinco.top ) {
+		if ($window.scrollTop()+40 >= contentCinco.top + ( windowHeight * 3 / 8 ) ) {
+			$('.ladera__section-item').addClass('reveal');
 			menuicon.removeClass('menuicon--white');
 			// menuicon.css('background-color', '#000');
 		}
-		// if ($window.scrollTop()-500 >= contentSeis.top ) {
+		// if ($window.scrollTop()+50 >= contentSeis.top + ( windowHeight * 7 / 8 ) ) {
 		// 	menuicon.css('background-color', '#fff');
 		// }
 
@@ -92,5 +95,14 @@
 		}		
 		console.log(e.target)
 	})
+
+
+	// ANIMATIONS
+
+	var windowHeight = $( window ).height();
+
+	if ($window.scrollTop() - ( windowHeight * 1 / 4 ) >= contentCinco.top ) {
+			$('.origen__proyecto--img').css('width', '180px');
+		}
 
 
